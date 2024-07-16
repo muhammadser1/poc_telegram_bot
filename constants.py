ec87a26e620a254ff361d648e82d30a24e597dbf
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-07-16 16:26:24 +0300

    change server url

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-SERVER_URL = "http://localhost:8000"
-pokemon_url= "http://localhost:8000/pokemon/"
+SERVER_URL = "https://poc-telegram-server-xcdw.onrender.com/"
+pokemon_url= "https://poc-telegram-server-xcdw.onrender.com/pokemon/"
